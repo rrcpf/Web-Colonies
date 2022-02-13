5181ffe6df65736bec1f63eb140a4149f6fc57e4
rrcpf <rrcpf2304@gmail.com>
2022-02-13 20:03:45 -0300

    Modificações no index.js

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 // módulos da plataforma
-import http from 'http'
 import express from 'express'
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 const app = express()
 
 app.set('port', PORT)
 
-app.use(express.static("."))
+app.use(express.static("./"))
 
 
 app.listen(PORT, () => {
-    console.log('Escutando em: http://localhost:3000')
+    console.log(`Escutando em: ${ PORT }`)
   })
 
 
